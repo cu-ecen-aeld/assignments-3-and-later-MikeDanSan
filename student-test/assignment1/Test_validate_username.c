5b94b0e3694c7a42e08c70d5ed1f3979234ecfb6
mike <mike@Ubuntu.myguest.virtualbox.org>
2025-04-09 16:46:59 -0400

    assignment1_username updated

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,5 +18,17 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+
+     // Call the hardcoded username function
+    const char* hardcoded_username = my_username();
+
+    // Call the function to get the username from the configuration file
+    char* config_username = malloc_username_from_conf_file();
+
+    // Assert that the two usernames are equal
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(hardcoded_username, config_username, "Usernames do not match!");
+
+    // Free the dynamically allocated memory
+    free(config_username);
+    // TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
 }
